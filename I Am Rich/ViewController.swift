07f7dec94df2e78f890e611b9738988452c43a38
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-10-10 19:24:23 +0700

    init

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yusup Saepudin on 10/10/18.
+//  Copyright © 2018 Yusup Saepudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
